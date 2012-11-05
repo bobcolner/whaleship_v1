e7ed78f401513e463fba5e4f26a6f762641fbfe9
bcolner <bcolner@ngmoco.com>
2012-11-04 20:27:14 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhaleShip::Application.config.secret_token = 'd2c69201bbaf40fed379c2ac62d270118400120df6c6db91c35706813ffdeb3073ca15bd25281768e412b2d9f3642498942bf97d905bf027f551775e30b5cf7a'
